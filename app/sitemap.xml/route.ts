228f1186c6f1d2af06a3deb644c9c721442b7a3a
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 00:25:23 +0530

    how it works

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,9 +1,8 @@
 export async function GET() {
   const url = "https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata";
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 60 } });
   const data = await res.json();
 
   return new Response(data.dateTime);
 }
 export const dynamic = "force-dynamic";
-export const revalidate = 120;
