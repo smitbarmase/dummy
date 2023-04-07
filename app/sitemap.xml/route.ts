0a2d4a2827db52bd83e678d6f8ac980830c3a4e9
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 00:03:44 +0530

    ok

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,9 +1,9 @@
 export async function GET() {
-  const url = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
-  const res = await fetch(url, {
-    next: { revalidate: 120 },
-  });
-  const data = await res.json();
+  // const url = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+  // const res = await fetch(url, {
+  //   next: { revalidate: 120 },
+  // });
+  // const data = await res.json();
 
-  return new Response(data.datetime);
+  return new Response("dsff");
 }
