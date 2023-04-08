99fbd6e7dd9bb29343132058230ba77d78a31bff
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 08:23:38 +0530

    dynamic time

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,8 +1,9 @@
 export async function GET() {
-  const url = "https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata";
+  const url = "https://71be-103-214-63-146.ngrok-free.app";
   const res = await fetch(url, { next: { revalidate: 60 } });
   const data = await res.json();
 
   return new Response(data.dateTime);
 }
+
 export const dynamic = "force-dynamic";
