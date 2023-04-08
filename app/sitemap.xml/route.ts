a50dd09c99012d69bcb3f9d17309c0791e302284
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 08:35:56 +0530

    nice

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
   const url = "https://71be-103-214-63-146.ngrok-free.app";
   const res = await fetch(url, { next: { revalidate: 60 } });
-
+  console.log("nice");
   return new Response(res.body);
 }
 
