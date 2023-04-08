2381cafdb43a14e30cea7526643dda42d0d5cfe4
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 08:29:39 +0530

    fix

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,9 +1,8 @@
 export async function GET() {
   const url = "https://71be-103-214-63-146.ngrok-free.app";
   const res = await fetch(url, { next: { revalidate: 60 } });
-  const data = await res.json();
 
-  return new Response(data);
+  return new Response(res.body);
 }
 
 export const dynamic = "force-dynamic";
