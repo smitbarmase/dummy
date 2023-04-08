808eb00670a9c1c9a4da6745f935c07d86cc80fe
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 09:16:23 +0530

    lets see

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,4 @@
 export async function GET() {
-  console.log("server called");
   return new Response(
     new Date().toLocaleString("en-US", {
       timeZone: "Asia/Kolkata",
--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,5 +1,4 @@
 export async function GET() {
-  console.log("sitemap called");
   const res = await fetch("https://dummy-orcin.vercel.app/api", {
     next: { revalidate: 60 },
   });
