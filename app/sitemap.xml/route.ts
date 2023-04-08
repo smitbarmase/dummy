1fb0f4bef7772dde8d8d31db3d574367111aa559
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 08:26:49 +0530

    better data

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -3,7 +3,7 @@ export async function GET() {
   const res = await fetch(url, { next: { revalidate: 60 } });
   const data = await res.json();
 
-  return new Response(data.dateTime);
+  return new Response(data);
 }
 
 export const dynamic = "force-dynamic";
