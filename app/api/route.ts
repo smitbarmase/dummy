21c23e94c3dfc4337316565e8ab24bf2aff26b54
Smit Barmase <heysmitbarmase@gmail.com>
2023-04-08 08:58:09 +0530

    time zone

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,8 @@
 export async function GET() {
   console.log("server called");
-  return new Response(new Date().toLocaleString("en-US"));
+  return new Response(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
 }
 
 export const dynamic = "force-dynamic";
